0cf9ea36c1bdc9b6b41824f37e0443f5734b2d2b
Michael Köppl <mko.mko31@gmail.com>
2015-08-13 16:14:33 +0200

    Add help option

--- src/client.c
+++ src/client.c
@@ -9,14 +9,21 @@
 #include <string.h>
 #define BUF 1024
 int main (int argc, char **argv) {
+  if(argc < 2){
+     printf("Usage: %s <server address>\n", *argv);
+     return -1;
+  }
+  if(strcmp(argv[1],"--help") == 0 || strcmp(argv[1], "-h") == 0)
+  {
+    printf("Usage: %s <server address>\n", *argv);
+    printf("Commands:\n\t/q ... quit\n");
+    return 0;
+  }
+
   int conn_socket;
   char *buffer = malloc (BUF);
   struct sockaddr_in address;
   int size;
-  if( argc < 2 ){
-     printf("Usage: %s <server address>\n", *argv);
-     return -1;
-  }
   if ((conn_socket=socket (AF_INET, SOCK_STREAM, 0)) < 0)
   {
     printf("Could not create socket.\n");
