335f7050d7f8519eda65544113a9bc7d4a35d545
Michael Köppl <mko.mko31@gmail.com>
2015-08-18 23:38:26 +0200

    Use IP entered by user instead of localhost

--- src/client.py
+++ src/client.py
@@ -3,5 +3,5 @@ import socket
 ip = raw_input("Enter ip address: ")
 msg = raw_input("Enter message:")
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('localhost', 7891))
+clientsocket.connect((ip, 7891))
 clientsocket.send(msg+'\n')
